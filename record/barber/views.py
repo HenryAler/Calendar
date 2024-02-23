69b631953c4d1dbe27c1a90111579f63d57202c1
HenryAler <Anderson-fin@mail.ru>
2024-02-23 22:50:33 +0300

    added options for displaying months

--- record/barber/views.py
+++ record/barber/views.py
@@ -23,18 +23,19 @@ def month_translation(month_name):
 
     """ Функция перводит названия месяцев на русский язык """
 
-    dict_month = {'January': 'Январь ',
-                  'February': 'Февраль ',
-                  'March': 'Март ',
-                  'April': 'Апрель ',
-                  'May': 'Май ',
-                  'June': 'Июнь ',
-                  'July': 'Июль ',
-                  'August': 'Август ',
-                  'September': 'Сентябрь ',
-                  'October': 'Октябрь ',
-                  'November': 'Ноябрь ',
-                  'December': 'Декабрь '}
+    dict_month = {'January': ('Январь', 'Января'),
+                  'February': ('Февраль', 'Февраля'),
+                  'March': ('Март', 'Марта'),
+                  'April': ('Апрель', 'Апреля'),
+                  'May': ('Май', 'Мая'),
+                  'June': ('Июнь', 'Июня'),
+                  'July': ('Июль', 'Июля'),
+                  'August': ('Август', 'Августа'),
+                  'September': ('Сентябрь', 'Сентября'),
+                  'October': ('Октябрь', 'Октября'),
+                  'November': ('Ноябрь', 'Ноября'),
+                  'December': ('Декабрь', 'Декабря')
+                  }
     
     return dict_month.get(month_name)
 
@@ -49,13 +50,13 @@ def view_calendar(request):
             month = 1
             year += 1
         month_name, date = get_data(year, month)
-        month_name = month_translation(month_name)
+        month_name = month_translation(month_name)[0]
         return render(request, 'calendar.html', {'date': date, 'month': month, 'title': 'calendar', 'year': year, 'month_name': month_name})
         
     year = time.localtime().tm_year
     month = time.localtime().tm_mon
     month_name, date = get_data(year, month)
-    month_name = month_translation(month_name)
+    month_name = month_translation(month_name)[0]
     return render(request, 'calendar.html', {'date': date,  'month': month, 'title': 'calendar', 'year': year, 'month_name': month_name})
     
 
@@ -64,7 +65,7 @@ def view_calendar(request):
 def barber_count(request, month, num):
 
     month = f'{calendar.month_name[month]}'
-    month = month_translation(month)
+    month = month_translation(month)[1]
     
 
     return render(request, 'barber.html', {'title': 'barber', 'num': num, 'month': month})
