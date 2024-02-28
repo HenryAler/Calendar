45d66673cbe97991f32b354902bff45eb664b33f
HenryAler <Anderson-fin@mail.ru>
2024-02-28 16:54:00 +0300

    reworking

--- record/barber/admin.py
+++ record/barber/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from barber.models import Barber, Record
+from barber.models import Barber
+
 
 @admin.register(Barber)
 class BarberAdmin(admin.ModelAdmin):
     list_display = ['name', 'surname']
 
 
-@admin.register(Record)
-class RecordAdmin(admin.ModelAdmin):
-    list_display = ['name', 'surname', 'fone', 'date', 'time', 'barber']
-    
-
 # admin.site.register(Barber)
-# admin.site.register(Records)
--- record/barber/models.py
+++ record/barber/models.py
@@ -12,24 +12,4 @@ class Barber(models.Model):
     
     class Meta():
         ordering = ['name']
-
-class Client(models.Model):
-    pass
-
-
-class Record(models.Model):
-
-    name = models.CharField(max_length=15)
-    surname = models.CharField(max_length=15)
-    fone = models.IntegerField()
-    email = models.EmailField(blank=True)
-    date = models.DateField()
-    time = models.TimeField()
-    barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-    
-
-    class Meta():
-        ordering = ['name']
+        
