e18b634aaf3592fedf510d54f1daf301a913682b
HenryAler <Anderson-fin@mail.ru>
2024-03-03 00:46:39 +0300

    working with html page barber, display barbers

--- record/barber/views.py
+++ record/barber/views.py
@@ -6,11 +6,13 @@ from .models import ScheduleBarber
 
 
 def barber_count(request, month, num, year):
-    
+
     num_month = '0' + str(month)
     month = f'{calendar.month_name[month]}'
     year = year
     month = month_translation(month)[1]
     barbers = ScheduleBarber.objects.filter(day=f'{year}-{num_month}-{num}')
+    for i in barbers:
+        print(i.barber.foto)
 
     return render(request, 'barber.html', {'title': 'barber', 'num': num, 'month': month, 'year': year, 'barbers': barbers})
--- record/record/settings.py
+++ record/record/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -126,5 +127,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
