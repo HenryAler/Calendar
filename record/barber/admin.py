3281abdf3f5d8642306a5edee2abd09089c2651d
HenryAler <Anderson-fin@mail.ru>
2024-03-02 18:21:22 +0300

    add models Schedule, reworking models Barber

--- record/barber/admin.py
+++ record/barber/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from barber.models import Barber
+from barber.models import Barber, ScheduleBarber
 
 
 @admin.register(Barber)
 class BarberAdmin(admin.ModelAdmin):
-    list_display = ['name', 'surname']
+    list_display = ['name', 'surname', 'getting_started', 'end_of_work']
 
 
-# admin.site.register(Barber)
+@admin.register(ScheduleBarber)
+class ScheduleAdmin(admin.ModelAdmin):
+    list_display = ['barber', 'day', 'working']
--- record/barber/models.py
+++ record/barber/models.py
@@ -1,15 +1,35 @@
 from django.db import models
+import datetime
 
 class Barber(models.Model):
 
     name = models.CharField(max_length=15, verbose_name='Имя')
     surname = models.CharField(max_length=15, verbose_name='Фамилия')
-    foto = models.ImageField(upload_to='Barber')
+    foto = models.ImageField(upload_to='Barber', verbose_name='Фото')
+    getting_started = models.TimeField(null=True, default=datetime.time(9, 0, 0), verbose_name='Начало работы')
+    end_of_work = models.TimeField(null=True, default=datetime.time(18, 0, 0), verbose_name='Окончание работы')
 
     def __str__(self):
-        # return f'{self.name} {self.surname}'
+
         return self.name
     
     class Meta():
+
         ordering = ['name']
+        verbose_name_plural = 'Мастера'
         
+
+class ScheduleBarber(models.Model):
+
+    day = models.DateField(null=True, verbose_name='День')
+    barber = models.ForeignKey(Barber, on_delete=models.CASCADE, verbose_name='Мастер')
+    working = models.BooleanField(default=True, verbose_name='Работает да/нет')
+
+    def __str__(self):
+
+        return 'Расписание работы мастеров'
+    
+    class Meta():
+
+        ordering = ['day']
+        verbose_name_plural = 'Расписание'
